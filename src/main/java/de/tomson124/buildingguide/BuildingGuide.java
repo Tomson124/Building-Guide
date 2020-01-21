21f43959acb99210500204a359dfefd264ef5ceb
Thomas <tomson124@outlook.de>
2020-01-21 22:48:17 +0100

    Adding mod files

---
+++ src/main/java/de/tomson124/buildingguide/BuildingGuide.java
@@ -0,0 +1,21 @@
+package de.tomson124.buildingguide;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = BuildingGuide.MODID, name = BuildingGuide.NAME, version = BuildingGuide.VERSION)
+public class BuildingGuide {
+	public static final String MODID = "buildingguide";
+	public static final String NAME = "Building Guide";
+	public static final String VERSION = "@VERSION@";
+
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+	}
+
+	@EventHandler
+	public void init(FMLInitializationEvent event) {
+	}
+}
